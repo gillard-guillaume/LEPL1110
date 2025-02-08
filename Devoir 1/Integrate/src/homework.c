405872b072f63128b219c944b32eef261e8fb8e1
Guillaume-gillard <gillard.guillaume@gmail.com>
2025-02-08 22:06:33 +0100

    first function finished

--- Devoir 1/Integrate/src/homework.c
+++ Devoir 1/Integrate/src/homework.c
@@ -3,23 +3,34 @@
 #include "glfem.h"
 
 
+// Guillaume Gillard
+// Igor Gregoire
+
+
 double integrate(double x[3], double y[3], double (*f) (double, double))
 {
     double I = 0;
     double xLoc[3];
     double yLoc[3];
 
-//
-// ... A modifier :-)
-//
-//
-// Pour dessiner l'element, les sommets du triangle :-)
-// Decommenter la ligne pour dessiner aussi les points d'integration
-//
+    double xi[3] = {1/6, 1/6, 2/3};
+    double eta[3] = {1/6, 2/3, 1/6};
+    double w[3] = {1/6, 1/6, 1/6};
 
-  glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
-  glfemSetColor(GLFEM_BLUE);  glfemDrawNodes(x,y,3);
-//  glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
+    
+    for (int i = 0; i < 3; i++)
+    {
+        xLoc[i] = x[0] * (1 - xi[i] - eta[i]) + x[1] * xi[i] + x[2] * eta[i];
+        yLoc[i] = y[0] * (1 - xi[i] - eta[i]) + y[1] * xi[i] + y[2] * eta[i];
+        I += w[i] * f(xLoc[i], yLoc[i]);
+    }
+
+
+
+
+    glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
+    glfemSetColor(GLFEM_BLUE);  glfemDrawNodes(x,y,3);
+    glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
     
 
 
