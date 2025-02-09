f01f60ddf7468e31088ce5be1cce47da78041984
gregoire.igor@gmail.com <Ms2vJpR98>
2025-02-09 10:05:32 +0100

    Devoir 1

--- Devoir 1/Integrate/src/homework.c
+++ Devoir 1/Integrate/src/homework.c
@@ -3,74 +3,75 @@
 #include "glfem.h"
 
 
-// Guillaume Gillard
-// Igor Gregoire
-
-
 double integrate(double x[3], double y[3], double (*f) (double, double))
 {
-    double I = 0;
-    double xLoc[3];
-    double yLoc[3];
+    double I = 0.0;
+    double xLoc[3] = {0.0, 0.0, 0.0};
+    double yLoc[3] = {0.0, 0.0, 0.0};
 
-    double xi[3] = {1/6, 1/6, 2/3};
-    double eta[3] = {1/6, 2/3, 1/6};
-    double w[3] = {1/6, 1/6, 1/6};
+    // magic
+    double xi[3] = {1.0/6, 1.0/6, 2.0/3};
+    double eta[3] = {1.0/6, 2.0/3, 1.0/6};
+    double w[3] = {1.0/6, 1.0/6, 1.0/6};
 
-
-    for (int i = 0; i < 3; i++)
-    // applying variable change on xi et eta and then the integration formula
-    {
-        xLoc[i] = x[0] * (1 - xi[i] - eta[i]) + x[1] * xi[i] + x[2] * eta[i];
-        yLoc[i] = y[0] * (1 - xi[i] - eta[i]) + y[1] * xi[i] + y[2] * eta[i];
-        I += w[i] * f(xLoc[i], yLoc[i]);
+    double J = fabs((x[1]-x[0])*(y[2]-y[0]) - (x[2]-x[0])*(y[1]-y[0]));
+    
+    for (int k=0; k<3; k++){
+        xLoc[k] = x[0] + xi[k]*(x[1]-x[0]) + eta[k]*(x[2]-x[0]);
+        yLoc[k] = y[0] + xi[k]*(y[1]-y[0]) + eta[k]*(y[2]-y[0]);
     }
 
+    for (int k=0; k<3; k++) I+= J*w[k]*f(xLoc[k],yLoc[k]);
 
-
-
-    glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
-    glfemSetColor(GLFEM_BLUE);  glfemDrawNodes(x,y,3);
-    glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
+  glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
+  glfemSetColor(GLFEM_BLUE);  glfemDrawNodes(x,y,3);
+  glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
     
-
-
     return I;
 }
 
-double integrateRecursive(double x[3], double y[3], double (*f)(double,double), int n)
-{
+double mitosis(double x[3], double y[3], double new_x[3], double new_y[3]){
+    for (int i=0; i<3; i++){
+        new_x[i] = 0.0;
+        new_y[i] = 0.0;
+    }
+    // Matrice en Raw Major
+    double P[9] = {1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0};
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            new_x[i] += P[3*i+j]*x[j]/2;
+            new_y[i] += P[3*i+j]*y[j]/2;
+        }
+    }
+    return 0;
+}
+
+double integrateRecursive(double x[3], double y[3], double (*f)(double,double), int n){
+
+    if (n==1) return integrate(x,y,f);
+
+    double new_x[3] = {0.0, 0.0, 0.0};
+    double new_y[3] = {0.0, 0.0, 0.0};
+    mitosis(x,y,new_x,new_y);
+
+    double x1[3] = {x[0], new_x[0], new_x[2]};
+    double y1[3] = {y[0], new_y[0], new_y[2]};
+
+    double x2[3] = {new_x[0], x[1], new_x[1]};
+    double y2[3] = {new_y[0], y[1], new_y[1]};
+
+    double x3[3] = {new_x[2], new_x[1], x[2]};
+    double y3[3] = {new_y[2], new_y[1], y[2]};
+
+    double x4[3] = {new_x[0], new_x[1], new_x[2]};
+    double y4[3] = {new_y[0], new_y[1], new_y[2]};
 
     double I = 0;
 
-    // decomposing into subtriangles by placing points on the middle of corners
-    if (n > 0)
-    {
-        double newpoints_x[3] = { (x[0] + x[1]) / 2, (x[1] + x[2]) / 2, (x[2] + x[0]) / 2 };
-        double newpoints_y[3] = { (y[0] + y[1]) / 2, (y[1] + y[2]) / 2, (y[2] + y[0]) / 2 };
-
-        triangle1_x = {x[0], newpoints_x[0], newpoints_x[2]};
-        triangle1_y = {y[0], newpoints_y[0], newpoints_y[2]};
-
-        triangle2_x = {newpoints_x[0], x[1], newpoints_x[1]};
-        triangle2_y = {newpoints_y[0], y[1], newpoints_y[1]};
-
-        triangle3_x = {newpoints_x[2], newpoints_x[1], x[2]};
-        triangle3_y = {newpoints_y[2], newpoints_y[1], y[2]};
-
-        triangle4_x = {newpoints_x[0], newpoints_x[1], newpoints_x[2]};
-        triangle4_y = {newpoints_y[0], newpoints_y[1], newpoints_y[2]};
-
-        I += integrateRecursive(triangle1_x, triangle1_y, f, n-1);
-        I += integrateRecursive(triangle2_x, triangle2_y, f, n-1);
-        I += integrateRecursive(triangle3_x, triangle3_y, f, n-1); 
-        I += integrateRecursive(triangle4_x, triangle4_y, f, n-1);
-    }
-    
-    else if (n == 0)
-    {
-        I += integrate(x,y,f);
-    }
+    I += integrateRecursive(x1,y1,f,n-1);
+    I += integrateRecursive(x2,y2,f,n-1);
+    I += integrateRecursive(x3,y3,f,n-1);
+    I += integrateRecursive(x4,y4,f,n-1);
 
     return I;
-}
+    }
